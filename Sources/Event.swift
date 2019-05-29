5f8dc5d422a4c4a12e7b4fefd908802d444e2c46
kingiol <kingxiaokang@gmail.com>
2019-05-29 18:18:17 +0800

    Fix \\.

--- Sources/Event.swift
+++ Sources/Event.swift
@@ -39,7 +39,7 @@ public struct Event {
     public var descr: String? {
         get {
             guard let key = otherAttrs.filterKeyHasPrefix("DESCRIPTION") else { return nil }
-            return otherAttrs[key]
+            return otherAttrs[key]?.replacingOccurrences(of: "\\", with: "")
         }
         set {
             guard let key = otherAttrs.filterKeyHasPrefix("DESCRIPTION") else {
